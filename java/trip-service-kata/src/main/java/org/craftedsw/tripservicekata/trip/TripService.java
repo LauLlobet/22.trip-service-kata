96d6aace20c1bbb08cdf2557b8031b884c679002
Lau Llobet <lau.llobet@gmail.com>
2018-05-14 16:50:20 +0200

    I realised the seam also needed to touch the getInstance in the legacy code, As far as I can imagine, there's the possibility to do it only in one point allowing the singleton to operate on the rest of the legacy system.
    Knowing this, what are the benefits of the seam instead of the stub? I haven't reach some degree of compatibility.
    
    I'll go backwards and try to reach it so.

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -9,9 +9,9 @@ import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+	public List<Trip> getTripsByUser(User user, UserSession userSession) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+		User loggedUser = userSession.getLoggedUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceCharectirizationsTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceCharectirizationsTest.java
@@ -1,5 +1,6 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.CollaboratorCallException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 import org.junit.Test;
@@ -15,12 +16,17 @@ public class TripServiceCharectirizationsTest {
 
     @Test
     public void
-    x() {
+    provide_no_trips_for_unrelated_firends() {
         TripService tripService = new TripService();
-        User user = null;
-        List<Trip> listOfTrips = tripService.getTripsByUser(user);
-        assertNull(listOfTrips);
+        User user = new User();
+        List<Trip> listOfTrips = tripService.getTripsByUser(user, new UserSession(){
+            public User getLoggedUser() {
+                return new User();
+            }
+        });
+        assertThat(listOfTrips.size(), is(0));
     }
+
 
 	
 }
