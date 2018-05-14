2ff0a13f28eecbbbd562e330e3639d5ec1fc8ab9
Lau Llobet <lau.llobet@gmail.com>
2018-05-14 16:20:35 +0200

    Atempting to inherit the singleton which would return a Mock when getInstance is called. This is not a good idea since we add a useless class ( the "kind of seam" that mocks itself ) I see two options:
    - do a seam that returns a user which would be provided in the test scope when get logged is called
    implies:
              - injecting a subclass in the legacy code, that is changing untested code
              - verbose testing
    
    - do a mock
              - injecting a mock in the legacy code, that is changing untested code
              - assuming the mocked class has only one point of acces (function) so legacy code must be revised if the singleton has more than one function.
              - As it is a singleton right now it implies to inject the instance, change each call to getInstance at all the code
    
    My decision will be to use a seam.

---
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/user/UserSessionSeam.java
@@ -0,0 +1,11 @@
+package org.craftedsw.tripservicekata.user;
+
+public class UserSessionSeam extends UserSession {
+
+    private UserSessionSeam() {
+        super();
+    }
+
+    private Use
+
+}
